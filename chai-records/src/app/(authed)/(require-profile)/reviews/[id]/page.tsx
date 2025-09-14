c0872d79feb0ac2347c4acafaa4d22e97646fad4
Kyle Marshall (u22570285) <u22570285@tuks.co.za>
2025-09-14 20:13:55 +0200

    fix

--- chai-records/src/app/(authed)/(require-profile)/reviews/[id]/page.tsx
+++ chai-records/src/app/(authed)/(require-profile)/reviews/[id]/page.tsx
@@ -1,11 +1,11 @@
 import { ReviewsSection } from "@/customComponents/reviews/reviewsSection";
 
-export default function ReviewsByUserPage({
+export default async function ReviewsByUserPage({
   params,
 }: {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }) {
-  const { id } = params;
+  const { id } = await params;
 
   return (
     <div className="mx-auto w-full max-w-5xl p-4 md:p-6">
